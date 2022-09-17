f0dfe3a86ce20fd1a11b2e07d9dca0de653d26ea
SarunasJ-gif <sarunasj82@gmail.com>
2022-09-17 15:04:19 +0300

    sqlite changed to postgres

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ import requests
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///movieTOP.db"
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///movieTOP.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 Bootstrap(app)
 db = SQLAlchemy(app)
